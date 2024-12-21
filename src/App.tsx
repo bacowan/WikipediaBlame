8bd3281762516c6fa45e64a269ccf12e64140d46
Brendan Cowan <bacowan@ualberta.ca>
2024-12-21 17:36:45 +0900

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Wikipedia Blame</h1>
+      <div className='search-area'>
+        <label>
+          Article Name: <input/>
+        </label>
+        <button className='blame-button'>
+          Blame
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='rev-area'>
+        <textarea readOnly className='main-left'/>
+        <div className='main-right'/>
+      </div>
     </>
   )
 }
