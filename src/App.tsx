8d7d740a32b82e74236fd76b8d04db4b8cafcd29
Brendan Cowan <bacowan@ualberta.ca>
2024-12-24 13:07:10 +0900

    Seems to work well for additions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const [articleName, setArticleName] = useState("");
   const [articleSource, setArticleSource] = useState<BlameItem[]>([]);
   const [selectedRevision, setSelectedRevision] = useState<Revision | null>(null);
+  const [hoveredRevision, setHoveredRevision] = useState<Revision | null>(null);
 
   async function Blame(name: string) {
     const revisions = await getRevisionsForArticle(name);
@@ -20,7 +21,7 @@ function App() {
       // TODO: Error handling
     }
     else {
-      const blames = getBlameItems(revisions.value[1], revisions.value[0]);
+      const blames = getBlameItems(revisions.value);
       setArticleSource(blames);
     }
   }
@@ -30,8 +31,10 @@ function App() {
       key={i}
       blameItem={b}
       isSelectedRevision={b.revision != null && selectedRevision != null && b.revision.id === selectedRevision.id}
-      setSelectedRevision={setSelectedRevision}/>)
-  }, [articleSource, selectedRevision]);
+      isHoveredRevision={b.revision != null && hoveredRevision != null && b.revision.id === hoveredRevision.id}
+      setSelectedRevision={setSelectedRevision}
+      setHoveredRevision={setHoveredRevision}/>)
+  }, [articleSource, selectedRevision, hoveredRevision]);
 
   return (
     <>
@@ -58,13 +61,108 @@ function App() {
   )
 }
 
-function getBlameItems(oldRev: Revision, newRev: Revision): BlameItem[] {
+function getBlameItems(revisions: Revision[]): BlameItem[] {
+  const latestRev = revisions[0];
+  let blameItems: BlameItem[] = [{
+    text: revisions[0].content,
+    revision: revisions[0],
+    type: "unchanged"
+  }]
+
+  for (let i = 1; i < revisions.length; i++) {
+    const olderRev = revisions[i];
+    const newerRev = revisions[i - 1];
+    blameItems = getBlameItem(blameItems, olderRev, newerRev, latestRev);
+  }
+
+  return blameItems;
+}
+
+/*function getLatestBlameItems(oldRev: Revision, newRev: Revision): BlameItem[] {
   const diff = Diff.diffChars(oldRev.content, newRev.content);
   return diff.map(d => ({
     text: d.value,
     type: d.added ? "add" : d.removed ? "remove" : "unchanged",
     revision: d.added || d.removed ? newRev : null
   }));
+}*/
+
+interface ArrayCharIndex {
+  itemIndex: number,
+  charIndex: number
+}
+
+function getBlameItem(blameItems: BlameItem[], olderRev: Revision, newerRev: Revision, latestRev: Revision): BlameItem[] {
+  let newBlameItems: BlameItem[] = [];
+  const diff: BlameItem[] = Diff.diffChars(olderRev.content, latestRev.content) // TODO: call with callback for async
+                    .map(d => ({
+                      text: d.value,
+                      type: d.added ? "add" : d.removed ? "remove" : "unchanged",
+                      revision: null
+                    }));
+
+  let blameIndex = { itemIndex: 0, charIndex: 0 }
+  let diffIndex = { itemIndex: 0, charIndex: 0 }
+  
+  while (blameIndex.itemIndex < blameItems.length) {
+    const blameItem = blameItems[blameIndex.itemIndex];
+    const diffItem = diff[diffIndex.itemIndex];
+    if (blameItem.type === "unchanged") {
+      if (diffItem.type === "remove") {
+        newBlameItems.push({
+          text: "",
+          type: "remove",
+          revision: newerRev
+        });
+        diffIndex.charIndex = 0;
+        diffIndex.itemIndex++;
+      }
+      else {
+        const textLength = Math.min(
+          blameItem.text.length - blameIndex.charIndex,
+          diffItem.text.length - diffIndex.charIndex);
+        newBlameItems.push({
+          text: blameItem.text.slice(blameIndex.charIndex, blameIndex.charIndex + textLength),
+          type: diffItem.type,
+          revision: diffItem.type === "add" ? newerRev : null
+        });
+        blameIndex = advanceIndex(blameIndex, blameItems, textLength);
+        diffIndex = advanceIndex(diffIndex, diff, textLength);
+      }
+    }
+    else {
+      newBlameItems.push(blameItem);
+      blameIndex = advanceIndex(blameIndex, blameItems, blameItem.text.length);
+      diffIndex = advanceIndex(diffIndex, diff, blameItem.text.length);
+    }
+  }
+
+  return newBlameItems;
+}
+
+function advanceIndex(oldIndex: ArrayCharIndex, array: BlameItem[], count: number): ArrayCharIndex {
+  let itemIndex = oldIndex.itemIndex;
+  let charIndex = oldIndex.charIndex;
+
+  charIndex += count;
+
+  try {
+  while (itemIndex < array.length && (
+          array[itemIndex].type === "remove" ||
+          charIndex >= array[itemIndex].text.length
+        )) {
+    if (array[itemIndex].type !== "remove") {
+      charIndex -= array[itemIndex].text.length;
+    }
+    itemIndex++;
+  }
+    
+}
+catch (e) {
+  console.log(e);
+}
+
+  return { itemIndex, charIndex };
 }
 
 async function getRevisionsForArticle(articleName: string) : Promise<Result<Revision[], string>> {
--- src/DiffElement.tsx
+++ src/DiffElement.tsx
@@ -5,10 +5,12 @@ import Revision from "./Revision";
 interface DiffElementProps {
     blameItem: BlameItem,
     isSelectedRevision: boolean,
+    isHoveredRevision: boolean,
     setSelectedRevision: (revision: Revision | null) => void,
+    setHoveredRevision: (revision: Revision | null) => void
 }
 
-function DiffElement({ blameItem, isSelectedRevision, setSelectedRevision }: DiffElementProps) {
+function DiffElement({ blameItem, isSelectedRevision, isHoveredRevision, setSelectedRevision, setHoveredRevision }: DiffElementProps) {
     let text;
     if (blameItem.type === "remove") {
       text = "\u00A0\u00A0\u00A0";
@@ -21,8 +23,18 @@ function DiffElement({ blameItem, isSelectedRevision, setSelectedRevision }: Dif
         setSelectedRevision(blameItem.revision);
     }
 
+    function onMouseEnter() {
+      setHoveredRevision(blameItem.revision);
+    }
+
+    function onMouseLeave() {
+      setHoveredRevision(null);
+    }
+
     return <span
-            className={`blame-${blameItem.type} ${isSelectedRevision ? "selected" : ""}`}
+            className={`blame-${blameItem.type} ${isSelectedRevision ? "selected" : ""} ${isHoveredRevision ? "hovered" : ""}`}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
             onClick={onClick}>
         {text}
     </span>
